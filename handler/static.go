6b2864588aac9eda329a3f7b1adecaac25d6d271
n-inja <mccreepertakumi@gmail.com>
2019-03-24 12:11:57 +0900

    static

--- handler/static.go
+++ handler/static.go
@@ -42,12 +42,12 @@ func init() {
 	}
 }
 
-func summarize(content string) string {
+func summarize(content []rune) string {
 	length := len(content)
 	if length > 32 {
 		length = 32
 	}
-	return strings.Replace(content[0:length], "\"", "", 0)
+	return strings.Replace(string(content[0:length]), "\"", "", 0)
 }
 
 func returnNotFound(c *gin.Context) {
@@ -117,7 +117,7 @@ func SetUser(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"url":         "https://" + hostname + "/blog/users/" + user.Name,
 		"title":       user.Name,
-		"description": summarize(user.Description),
+		"description": summarize([]rune(user.Description)),
 		"imageURL":    imageURL,
 	})
 }
@@ -134,7 +134,7 @@ func SetProject(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"url":         "https://" + hostname + "/blog/projects/" + projectName,
 		"title":       project.Name,
-		"description": summarize(project.Description),
+		"description": summarize([]rune(project.Description)),
 		"imageURL":    "https://" + hostname + "/static/favicon.png",
 	})
 }
@@ -181,7 +181,7 @@ func SetPost(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"url":         "https://" + hostname + "/blog/projects/" + projectName + "/post/" + strconv.Itoa(number),
 		"title":       project.Name + " - " + post.Title,
-		"description": summarize(post.Content),
+		"description": summarize([]rune(post.Content)),
 		"imageURL":    imageURL,
 	})
 }
